a7a16deb729bcd6db961bc1cf215a24530148476
Neil Thomson <neil.a.thomson@hotmail.com>
2024-01-17 14:55:26 +0000

    Add email and phone fallbacks

--- src/app/(default)/contact.tsx
+++ src/app/(default)/contact.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import {
-  FaEnvelope, FaLink, FaLinkedinIn, FaLocationDot, FaGithub,
+  FaEnvelope, FaLink, FaLinkedinIn, FaLocationDot, FaGithub, FaPhone,
 } from 'react-icons/fa6';
 import './contact.css';
 
@@ -14,52 +14,69 @@ interface ContactItemProps {
 const ContactItem: React.FC<ContactItemProps> = ({
   icon: Icon, title, href, display,
 }) => (
-    <div className="contact-item">
-      <div className="contact-item-icon-container">
-        <Icon className="contact-item-icon" />
-      </div>
-      <div className="contact-item-details">
-        <span className="contact-item-title">{title}</span>
-        {href != null
-          ? <a className="contact-item-value" href={href}>{display}</a>
-          : <span className="contact-item-value">{display}</span>
-        }
-      </div>
+  <div className="contact-item">
+    <div className="contact-item-icon-container">
+      <Icon className="contact-item-icon"/>
     </div>
+    <div className="contact-item-details">
+      <span className="contact-item-title">{title}</span>
+      {href != null
+        ? <a className="contact-item-value" href={href}>{display}</a>
+        : <span className="contact-item-value">{display}</span>
+      }
+    </div>
+  </div>
 );
 
 const Contact: React.FC = () => (
-    <div className="contact-container">
+  <div className="contact-container">
+    {process.env.CONTACT_EMAIL ? (
+      <ContactItem
+        icon={FaEnvelope}
+        title={'Email'}
+        href={`mailto:${process.env.CONTACT_EMAIL}`}
+        display={process.env.CONTACT_EMAIL}
+      />
+    ) : (
       <ContactItem
         icon={FaEnvelope}
         title={'Contact'}
         href={`${process.env.SITE_URL}/contact`}
         display={'neilthomson.dev/contact'}
       />
+    )}
+    {process.env.CONTACT_PHONE && (
       <ContactItem
-        icon={FaLink}
-        title={'Website'}
-        href={`${process.env.SITE_URL}`}
-        display={'neilthomson.dev'}
+        icon={FaPhone}
+        title={'Phone'}
+        href={`tel:${process.env.CONTACT_PHONE}`}
+        display={process.env.CONTACT_PHONE}
       />
-      <ContactItem
-        icon={FaLinkedinIn}
-        title={'LinkedIn'}
-        href={'https://www.linkedin.com/in/neil-thomson-51195a225/'}
-        display={'neil-thomson-51195a225'}
-      />
-      <ContactItem
-        icon={FaGithub}
-        title={'GitHub'}
-        href={'https://github.com/neilathomson'}
-        display={'neilathomson'}
-      />
-      <ContactItem
-        icon={FaLocationDot}
-        title={'Address'}
-        display={'Ipswich, UK'}
-      />
-    </div>
+    )}
+    <ContactItem
+      icon={FaLink}
+      title={'Website'}
+      href={`${process.env.SITE_URL}`}
+      display={'neilthomson.dev'}
+    />
+    <ContactItem
+      icon={FaLinkedinIn}
+      title={'LinkedIn'}
+      href={'https://www.linkedin.com/in/neil-thomson-51195a225/'}
+      display={'neil-thomson-51195a225'}
+    />
+    <ContactItem
+      icon={FaGithub}
+      title={'GitHub'}
+      href={'https://github.com/neilathomson'}
+      display={'neilathomson'}
+    />
+    <ContactItem
+      icon={FaLocationDot}
+      title={'Address'}
+      display={'Ipswich, UK'}
+    />
+  </div>
 );
 
 export default Contact;
