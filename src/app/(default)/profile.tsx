09a0e80ec87649c0cfbe9c98f201f219ce4f838f
Neil Thomson <neil.a.thomson@hotmail.com>
2024-01-23 13:26:36 +0000

    Added tagline, name, and image to About table

--- src/app/(default)/profile.tsx
+++ src/app/(default)/profile.tsx
@@ -1,16 +1,18 @@
 import React from 'react';
 import './profile.css';
+import prisma from '@/services/prisma';
 
-const Profile: React.FC = () => (
+const Profile: React.FC = async () => {
+  const about = await prisma.about.findFirst();
+  return (
     <div className="profile-container">
-      <img className="profile-image" src="images/neil-thomson.jpg" alt="" />
-      <span className="profile-title">Neil Thomson</span>
-      <span className="profile-sub-title">Software Developer</span>
-      <span className="profile-sub-title">Team Lead</span>
-      <span className="profile-sub-title">Product Enthusiast</span>
+      {about?.image && <img className="profile-image" src={about.image} alt=""/>}
+      {about?.name && <span className="profile-title">{about.name}</span>}
+      {about?.tagline && <span className="profile-sub-title">{about.tagline}</span>}
       <span className="profile-quote">First, solve the problem. Then, write the code.</span>
       <span className="profile-quote-author">- John Johnson</span>
     </div>
-);
+  );
+};
 
 export default Profile;
