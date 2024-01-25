b8d5066b32db5ac961cd95094b7ba17a24e23e81
Neil Thomson <neil.a.thomson@hotmail.com>
2024-01-25 14:01:03 +0000

    Fix tailwind config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -4,9 +4,8 @@ const { nextui } = require('@nextui-org/react');
 
 const config: Config = {
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
     './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/admin/**/*.{js,ts,jsx,tsx,mdx}',
     './node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
